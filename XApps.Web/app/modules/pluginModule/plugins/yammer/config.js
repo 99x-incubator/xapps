88c7716ed42c94599b33cab90c7a3d2e2acd572a
Achini Adikari <achini89@live.com>
2013-05-30 17:14:16 +0530

    Updated URLs

--- XApps.Web/app/modules/pluginModule/plugins/yammer/config.js
+++ XApps.Web/app/modules/pluginModule/plugins/yammer/config.js
@@ -8,7 +8,7 @@
         "name": "Yammer",
         "tileSize": "small", //Can be 'large' or 'small'
         "tileColor": "dark_blue", //Color of your tile choose from the collection ("blue", "dark_blue", "green", "dark_green", "red", "yellow", "orange", "pink", "purple", "black")
-        "target": "https://www.yammer.com/",
+        "target": "https://www.yammer.com/99x.lk",
         "description": "Expand your network at work",
         "category": "social_media"
     };
