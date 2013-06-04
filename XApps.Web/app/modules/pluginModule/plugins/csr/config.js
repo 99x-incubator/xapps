01dad46b194671ccf2ce8c9d01c5f918701f77d1
Achini Adikari <achini89@live.com>
2013-06-04 12:58:05 +0530

    Added new plugins

--- XApps.Web/app/modules/pluginModule/plugins/csr/config.js
+++ XApps.Web/app/modules/pluginModule/plugins/csr/config.js
@@ -7,7 +7,7 @@
         "pluginId": "csr", //This should be unique id for your plugin
         "name": "CSR @ 99X",
         "tileSize": "small", //Can be 'large' or 'small'
-        "tileColor": "white", //Color of your tile choose from the collection ("blue", "dark_blue", "green", "dark_green", "red", "yellow", "orange", "pink", "purple", "black")
+        "tileColor": "dark_green", //Color of your tile choose from the collection ("blue", "dark_blue", "green", "dark_green", "red", "yellow", "orange", "pink", "purple", "black")
         "target": "https://www.facebook.com/99XTechnologyCSR",
         "description": "CSR Foundation at 99X Technology... Making a Difference",
         "category": "99x_places"
