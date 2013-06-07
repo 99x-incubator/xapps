354e31795b232c69369fad55529b0a79ab09f90a
Achini Adikari <achini89@live.com>
2013-06-07 10:53:08 +0530

    Changed URLs of Jira and Dataport system

--- XApps.Web/app/modules/pluginModule/plugins/jira/config.js
+++ XApps.Web/app/modules/pluginModule/plugins/jira/config.js
@@ -8,7 +8,7 @@
         "name": "99X Jira",
         "tileSize": "small", //Can be 'large' or 'small'
         "tileColor": "purple", //Color of your tile choose from the collection ("blue", "dark_blue", "green", "dark_green", "red", "yellow", "orange", "pink", "purple", "black")
-        "target": "http://jira.99x.no/secure/Dashboard.jspa",
+        "target": "https://seranet.atlassian.net/login?dest-url=%2Fsecure%2FDashboard.jspa",
         "description": "JIRA is the project tracker for teams planning, building and launching great products.",
         "category": "99x_places"
     };
